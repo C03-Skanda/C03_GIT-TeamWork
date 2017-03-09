03608c0e06ef03cc004a7d9fad63daa989055dc5
Mskanda <mskanda@student.tgm.ac.at>
2017-03-09 13:47:52 +0100

    Nur Max

---
+++ src/skanda/HalloLeute.java
@@ -0,0 +1,12 @@
+package skanda;
+
+public class HalloLeute {
+	
+	public static void HalloMax(){
+		System.out.println("Hallo Max!");
+	}
+	
+	public static void main(String[] args){
+		HalloMax();
+	}
+}
